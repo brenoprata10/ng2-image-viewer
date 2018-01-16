b48d977cd54416215fa1b26a1fe163858b2273f8
brenoprata10 <breno.prata@basis.com.br>
2018-01-15 22:28:05 -0200

    Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BaseImage} from "./base-image";
+import {BasePDF} from "./basePDF";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  images = [];
+  formGroup: FormGroup;
+  tste;
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      'primaryColor': new FormControl('#0176bd'),
+      'buttonsColor': new FormControl('#333333'),
+      'defaultDownloadName': new FormControl('Image')
+    });
+    this.images.push(BaseImage.IMAGE);
+    this.images.push(BasePDF.PDF);
+  }
+
+  changePrimaryColor() {
+    $('.inline-icon').css('background-color', this.formGroup.get('primaryColor').value);
+    $('.footer-info').css('background-color', this.formGroup.get('primaryColor').value);
+  }
+
+  changeButtonsColor() {
+    $('.footer-icon').css('color', this.formGroup.get('buttonsColor').value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { ImageViewerModule } from 'ng2-image-viewer';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MaterializeModule, MzInputModule} from "ng2-materialize";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageViewerModule,
+    ReactiveFormsModule,
+    MzInputModule,
+    BrowserAnimationsModule,
+    MaterializeModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
